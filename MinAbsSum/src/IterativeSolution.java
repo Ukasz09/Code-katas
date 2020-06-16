cf755a5446d57ed478bfe701849bfb9ea092d76b
Ukasz09 <246703@student.pwr.edu.pl>
2020-06-16 10:09:46 +0200

    Improve correctness of interative solution

--- MinAbsSum/src/IterativeSolution.java
+++ MinAbsSum/src/IterativeSolution.java
@@ -1,18 +1,22 @@
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 
 public class IterativeSolution {
 
     private static int solution(int[] A) {
-        ArrayList<Integer> sum = new ArrayList<>();
+        if (A == null || A.length == 0)
+            return 0;
+
+        HashSet<Integer> sum = new HashSet<>();
         sum.add(A[0]);
         for (int i = 1; i < A.length; i++) {
-            ArrayList<Integer> tmpSum = new ArrayList<>();
+            HashSet<Integer> tmpSum = new HashSet<>();
             for (Integer s : sum) {
                 tmpSum.add(Math.abs(s + A[i]));
                 tmpSum.add(Math.abs(s - A[i]));
                 sum = tmpSum;
             }
+            System.out.println();
         }
         return Collections.min(sum);
     }
